4f1f86b8e10d20785253a0d3ee9a31f9bebaaf29
Emmanuel Arellano <marwel0108@gmail.com>
2022-02-15 08:23:51 -0600

    Command and ViewModel Added for the History View. Nav Logic added to it.

---
+++ ViewModels/HistoryViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DigitalPresc.Commands;
+using DigitalPresc.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DigitalPresc.ViewModels
+{
+    public class HistoryViewModel : ViewModelBase
+    {
+        public ICommand NavigationCommand { get; }
+
+        public HistoryViewModel(NavigationStore navigationStore)
+        {
+            NavigationCommand = new HomeNavCommand(navigationStore);
+        }
+    }
+}
